a95d9b10c6e1ec6c837f75404c74d5bea8dd099b
Blake C <bcortri1@kent.edu>
2022-08-31 13:34:07 -0400

    functions refactored

---
+++ app.js
@@ -0,0 +1,30 @@
+/* ORIGINAL CODE TO BE REFACTORED
+
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+}
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+}
+*/
+
+//REFACTORED CODE
+
+const double = (arr) => (arr.map( (val) => val * 2));
+
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map((num) => num ** 2);
+    var evens = squares.filter((square) => square % 2 === 0);
+    
+    return evens;
+}
