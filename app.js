fada2f2d062a0e67e6d8c846e8309ffb517cb2d0
Blake C <bcortri1@kent.edu>
2022-08-31 13:42:05 -0400

    square and find evens refactored further

--- app.js
+++ app.js
@@ -22,9 +22,4 @@ function squareAndFindEvens(numbers){
 const double = (arr) => (arr.map( (val) => val * 2));
 
 
-function squareAndFindEvens(numbers){
-    var squares = numbers.map((num) => num ** 2);
-    var evens = squares.filter((square) => square % 2 === 0);
-    
-    return evens;
-}
+const squareAndFindEvens = (numbers) => (numbers.map((num) => num ** 2).filter((square) => square % 2 === 0));
